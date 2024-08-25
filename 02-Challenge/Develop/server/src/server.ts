7ef1f121c875eb5c5449b4d8150f16dd67a4314c
Sammy Kordi <Sammy.Kordi@gmail.com>
2024-08-25 10:33:36 -0700

    Updated Server code

--- 02-Challenge/Develop/server/src/server.ts
+++ 02-Challenge/Develop/server/src/server.ts
@@ -11,10 +11,17 @@ const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
 
+app.use(express.static('dist'));
+
+
 // TODO: Implement middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 // TODO: Implement middleware to connect the routes
-app.use(routes);
+app.use('/api', routes);
+
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
